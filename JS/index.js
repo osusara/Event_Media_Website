347afdc8ba2d43b6464287906b9e679036c0ffa6
osusara <osusarakammalawatta@yahoo.com>
2018-09-17 10:23:49 +0530

    Corrected the issue of the image slider

--- JS/index.js
+++ JS/index.js
@@ -1,10 +1,9 @@
 var imgcount = 1;
-var total = 4;
  
 function slide(x){
     var image = document.getElementById('imge');
     if (x > 0) {
-    	if (imgcount < total) {
+    	if (imgcount < 4) {
     		imgcount = imgcount + x;
     		image.src = "Resources/img"+ imgcount + ".JPG"; 
     	}
