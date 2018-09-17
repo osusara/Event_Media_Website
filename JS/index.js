9f03d47c407e060828466e518fdde6eed2ae56ce
osusara <osusarakammalawatta@yahoo.com>
2018-09-17 10:22:34 +0530

    Updated

--- JS/index.js
+++ JS/index.js
@@ -3,8 +3,15 @@ var total = 4;
  
 function slide(x){
     var image = document.getElementById('imge');
-    if (imgcount <= total) {
-    	imgcount = imgcount + x;
-    	image.src = "Resources/img"+ imgcount + ".JPG"; 
+    if (x > 0) {
+    	if (imgcount < total) {
+    		imgcount = imgcount + x;
+    		image.src = "Resources/img"+ imgcount + ".JPG"; 
+    	}
+    }else{
+    	if (imgcount > 1) {
+    		imgcount = imgcount + x;
+    		image.src = "Resources/img"+ imgcount + ".JPG"; 
+    	}
     }
 }
