429b1add579cfc65e2e7f9ccabe8f9f0e50e78f8
osusara <osusarakammalawatta@yahoo.com>
2018-10-20 19:18:25 +0530

    Developed the slide show in Index.html

--- JS/index.js
+++ JS/index.js
@@ -1,16 +1,43 @@
-var imgcount = 1;
- 
-function slide(x){
-    var image = document.getElementById('imge');
-    if (x > 0) {
-    	if (imgcount < 4) {
-    		imgcount = imgcount + x;
-    		image.src = "Resources/img"+ imgcount + ".JPG"; 
-    	}
-    }else{
-    	if (imgcount > 1) {
-    		imgcount = imgcount + x;
-    		image.src = "Resources/img"+ imgcount + ".JPG"; 
-    	}
-    }
+let sliderImages = document.querySelectorAll('.slide'),
+	arrowLeft = document.querySelector('#arrow-left'),
+	arrowRight = document.querySelector('#arrow-right'),
+	current = 0;
+
+function reset(){
+	for(let i =0; i < sliderImages.length; i++){
+		sliderImages[i].style.display = 'none';
+	}
 }
+
+function startSlide(){
+	reset();
+	sliderImages[0].style.display = 'block';
+}
+
+function slideLeft(){
+	reset();
+	sliderImages[current - 1].style.display = 'block';
+	current--;
+}
+
+function slideRight(){
+	reset();
+	sliderImages[current + 1].style.display = 'block';
+	current++;
+}
+
+arrowLeft.addEventListener('click', function(){
+	if(current === 0){
+		current = sliderImages.length;
+	}
+	slideLeft();
+});
+
+arrowRight.addEventListener('click', function (){
+	if (current === sliderImages.length - 1) {
+		current = -1;
+	}
+	slideRight();
+});
+
+startSlide();
